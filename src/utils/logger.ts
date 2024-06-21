9130d362bb9e4d49472ae70247218463229ebb06
Joanneyoung01 <jo.young@digital.cabinet-office.gov.uk>
2024-06-21 15:46:58 +0100

    Add logger logic

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 import { pino } from "pino";
 
 export const logger = pino({
-  name: "@govuk-one-login/frontend-passthrough-headers",
+  name: "@govuk-one-login/frontend-vital-signs",
   level: process.env.LOG_LEVEL || process.env.LOGS_LEVEL || "warn",
 });
